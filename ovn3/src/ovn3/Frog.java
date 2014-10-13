cfaa220a0490b2f8f77d966ca52a23280baaa15a
Gabriel <ga22be@live.se>
2014-10-13 13:04:16 +0200

    Frog fixad

--- ovn3/src/ovn3/Frog.java
+++ ovn3/src/ovn3/Frog.java
@@ -1,5 +1,43 @@
 package ovn3;
 
 public class Frog {
-
+	private int x = 0;
+	private int y = 0;
+	private int jumps = 0;
+	
+	public Frog(){
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public int getJumps(){
+		return jumps;
+	}
+	
+	public void jumpNorth(){
+		y = y - 1;
+		jumps++;
+	}
+	
+	public void jumpSouth(){
+		y = y + 1;
+		jumps++;
+	}
+	
+	public void jumpWest(){
+		x = x - 1;
+		jumps++;
+	}
+	
+	public void jumpEast(){
+		x = x + 1;
+		jumps++;
+	}
+	
 }
