335e94b5876ae3e7a168b4ed17671f2e164959b5
Gabriel <ga22be@live.se>
2014-11-26 16:50:24 +0100

    Fixade greyscale i labb 8

--- lab8-9/src/lab8_9/GreyScaleFilterV2.java
+++ lab8-9/src/lab8_9/GreyScaleFilterV2.java
@@ -4,22 +4,22 @@ import se.lth.cs.ptdc.images.ImageFilter;
 
 /** IdentityFilter beskriver en identitetstransformation */
 public class GreyScaleFilterV2 extends ImageFilter {
+	Color[] greyScale = new Color[256];
 	public GreyScaleFilterV2(String name) {
 		super(name);
+		for(int i = 0; i < greyScale.length; i++){
+			greyScale[i] = new Color(i,i,i);
+		}
 	}
 	
 	public Color[][] apply(Color[][] inPixels, double paramValue) {
 		int height = inPixels.length;
 		int width = inPixels[0].length;
-		int intensity;
+		short[][] intensity = super.computeIntensity(inPixels);
 		Color[][] outPixels = new Color[height][width];
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				Color pixel = inPixels[i][j];	
-				intensity = Math.round(((pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3));
-				outPixels[i][j] = new Color(intensity, 
-											intensity,
-											intensity);
+				outPixels[i][j] = greyScale[intensity[i][j]];
 			}
 		}
 		return outPixels;
